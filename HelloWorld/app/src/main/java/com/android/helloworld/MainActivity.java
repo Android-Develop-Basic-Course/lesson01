fc7816c6ff88c8774a5f6861a18d479b14dfdd73
Sergiogonzalezpi <sergio.gonzalezpi@alumnos.upm.es>
2022-02-15 19:31:59 +0100

    Add logs

--- HelloWorld/app/src/main/java/com/android/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/android/helloworld/MainActivity.java
@@ -1,13 +1,16 @@
 package com.android.helloworld;
 
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        Log.d(LOG_TAG, "Hello World 2");
     }
 }
